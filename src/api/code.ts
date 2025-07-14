85c218f911f648e34614915db4ee8964b9ad8de6
hois <2562189338@qq.com>
2025-07-15 00:39:09 +0800

    运行代码已调通

--- src/api/code.ts
+++ src/api/code.ts
@@ -3,7 +3,7 @@ import { apiRequest } from '@/api/apiRequest';
 async function runCode(source_code: string, language_id: number, stdin: string): Promise<any> {
   return await apiRequest({
     method: 'POST',
-    url: '/code/run-code',
+    url: '/code/run-code/',
     data: { source_code, language_id, stdin },
   });
 }
